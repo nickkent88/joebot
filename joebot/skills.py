68155abc43491fa6584a43035cbb00256d140473
Nick Kent <nkent@ntia.doc.gov>
2017-11-01 18:09:40 -0600

    Add 'echo' skill

--- joebot/skills.py
+++ joebot/skills.py
@@ -2,17 +2,24 @@ from .joebot import JoeBot
 
 
 @JoeBot.handles('testing')
-def testing():
+def testing(bot, event):
     return 'No, you\'re testing me.'
 
 @JoeBot.handles('help')
-def help():
+def help(bot, event):
     pass
 
 @JoeBot.handles('show')
-def show():
+def show(bot, event):
     pass
 
+@JoeBot.handles('echo')
+def echo(bot, event):
+    message = event['text'].split(event['text'])[1].strip().lower()
+    command = message.split()[0]
+    content = message.lstrip(command)
+    bot.post_message(content)
+
 @JoeBot.handles('')
-def no_retort():
+def no_retort(bot, event):
     pass
